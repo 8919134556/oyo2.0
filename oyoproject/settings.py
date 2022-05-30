f354b1e299730545ebc1d8423e741ff2a4a35743
suryaanand nadendla <103498529+8919134556@users.noreply.github.com>
2022-05-30 11:46:03 +0530

    Update settings.py

--- oyoproject/settings.py
+++ oyoproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8=wke)gn9yflx+$l-e9e9*0&5542u4k9l00((2eivziv0+o3bh'
+SECRET_KEY = '****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,8 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'suryaanand975@gmail.com'
-EMAIL_HOST_PASSWORD = 'Anand@123'
+EMAIL_HOST_USER = '*************************'
+EMAIL_HOST_PASSWORD = '**********************'
 EMAIL_USE_TLS = True
 
 
@@ -150,7 +150,7 @@ EMAIL_USE_TLS = True
 #          'NAME': 'oyo_db',
 #          'USER': 'surya',
 #          'PASSWORD': 'surya123',
-#          'HOST': 'oyoproject.ccikch20zfwt.us-west-2.rds.amazonaws.com',
+#          'HOST': '************************',
 #          'port': '3306',
 #     }
 # }
